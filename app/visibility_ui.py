9d1e8826665a344d453e7b2704e346fc708e91fa
samitab <samitab@cisco.com>
2022-02-07 17:32:54 +1000

    progress #5 Changed Read me to reflect environ variables. Added the new environ variables. Adding support for Neo4j.

--- app/visibility_ui.py
+++ app/visibility_ui.py
@@ -1,8 +1,11 @@
 #!/usr/local/bin/python3
 from flask import Flask, render_template, request
-from  graph import vkaci_draw, vkaci_build_topology, vkaci_env_variables, apic_methods_resolve
+from  graph import vkaci_graph, vkaci_build_topology, vkaci_env_variables, apic_methods_resolve
+
 app = Flask(__name__, template_folder='template',static_folder='template/assets')
-topology = vkaci_build_topology(vkaci_env_variables(), apic_methods_resolve())
+env = vkaci_env_variables()
+topology = vkaci_build_topology(env, apic_methods_resolve())
+graph = vkaci_graph(env,topology)
 
 f = open("version.txt", "r")
 __build__ = f.read()
@@ -11,19 +14,24 @@ __build__ = f.read()
 
 def index():
     if request.method == 'POST':
-        if 'podname' in request.form:
-            # Only re-calculate topology if you press Submit
-            if request.form['submit_button'] == 'Submit':
-                pod_topo = vkaci_draw(topology.update())
-            else:
-                pod_topo = vkaci_draw(topology.get())
-            pod_topo.add_pod(request.form['podname'])
-            pod_topo.svg('template/assets/pod')
-        else:
-            cluster_topo = vkaci_draw(topology.update())
-            cluster_topo.add_nodes()
-            cluster_topo.svg('template/assets/cluster')
-        return render_template('index.html', version=__build__)
+
+        # Update neo4j with topology data using graph
+        graph.update_database()
+
+        # if 'podname' in request.form:
+        #     # Only re-calculate topology if you press Submit
+        #     if request.form['submit_button'] == 'Submit':
+        #         #pod_topo = vkaci_graph(topology.update())
+        #     else:
+        #         #pod_topo = vkaci_graph(topology.get())
+        #     # pod_topo.add_pod(request.form['podname'])
+        #     # pod_topo.svg('template/assets/pod')
+        # else:
+        #     #cluster_topo = vkaci_graph(topology.update())
+        #     # cluster_topo.add_nodes()
+        #     # cluster_topo.svg('template/assets/cluster')
+        # # return render_template('index.html', version=__build__)
+
     return render_template('index.html', version=__build__)
 
 if __name__ == '__main__':
