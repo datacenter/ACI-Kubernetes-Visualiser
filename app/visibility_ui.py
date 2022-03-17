438fcf77cce7cd1378fc5beffd6a668e19ada43b
samitab <samitab@cisco.com>
2022-03-17 10:31:55 +1000

    close #22 - Aoto regenarate on sart up.

--- app/visibility_ui.py
+++ app/visibility_ui.py
@@ -13,6 +13,8 @@ __build__ = f.read()
 
 topology.update()
 
+graph.update_database()
+
 @app.route('/')
 def index():
     return render_template('index.html', version=__build__, env=env, pod_names = topology.get_pods(), node_names = topology.get_nodes(), namespaces = topology.get_namespaces(), leaf_names = topology.get_leafs())
