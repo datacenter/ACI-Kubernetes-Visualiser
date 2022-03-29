8479ff87be9a203ebc74d60b3ac0db4cbc224a07
samitab <samitab@cisco.com>
2022-03-29 14:05:54 +1000

    Fix #29 - Stop table being rendered multiple times. -And only appear when topology tab is clicked

--- app/template/assets/cui-2.0.0/js/vkaci-table.js
+++ app/template/assets/cui-2.0.0/js/vkaci-table.js
@@ -1,32 +1,28 @@
 
-var photo = function (obj) {
-  if (obj.image)
-    return "{common.icon()} <img src=./assets/cui-2.0.0/img/#image# width=16 height=16 style='margin:3px 4px 0px 1px;'><span>#value#</span>";
-  return "";
-};
+var gridd = null
 
-var gridd = {
-  view: "treetable",
-  css: "webix_dark",
-  container: "table",
-  resizeColumn: true, resizeRow: true,
-  columns: [
-    // { id: "id", header: "", css: { "text-align": "right" }, width: 50 },
-    {
-      id: "value", header: ["Name", { content: "textFilter" }], width: 300,
-      template: "{common.icon()} <img src=./assets/cui-2.0.0/img/#image# width=16 height=16 style='margin:3px 4px 0px 1px;'><span>#value#</span>",
-    },
-    { id: "ns", header: ["Namespace", { content: "selectFilter" }], width: 150 },
-    { id: "ip", header: ["IP", { content: "textFilter" }], width: 200 },
-    { id: "interface", header: ["Interface", { content: "selectFilter" }], width: 200 },
-  ],
+function renderTable() {
+  if (gridd === null) {
+    gridd = {
+      view: "treetable",
+      css: "webix_dark",
+      container: "table",
+      resizeColumn: true, resizeRow: true,
+      columns: [
+        {
+          id: "value", header: ["Name", { content: "textFilter" }], width: 300,
+          template: "{common.icon()} <img src=./assets/cui-2.0.0/img/#image# width=16 height=16 style='margin:3px 4px 0px 1px;'><span>#value#</span>",
+        },
+        { id: "ns", header: ["Namespace", { content: "selectFilter" }], width: 150 },
+        { id: "ip", header: ["IP", { content: "textFilter" }], width: 200 },
+        { id: "interface", header: ["Interface", { content: "selectFilter" }], width: 200 },
+      ],
 
-  autoheight: true,
-  scroll: false,
-  url: "/table_data", datatype: "json"
-};
+      autoheight: true,
+      scroll: false,
+      url: "/table_data", datatype: "json"
+    };
 
-webix.ui(gridd);
-
-
-
+    webix.ui(gridd);
+  }
+}
