703406a4cc9a206d5cc13709f571e46072fdf100
samitab <samitab@cisco.com>
2022-05-20 15:30:16 +1000

    Fix #47 Updated version tag

--- app/build_push.sh
+++ app/build_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+TAG="1.0rc2"
 echo -n "Build: " > version.txt
 echo -n `git describe --tags --always HEAD` >> version.txt
 echo -n " " >> version.txt
 date >> version.txt
-docker build -t quay.io/camillo/vkaci-init:1.0rc0 --target=vkaci-init  . &&  docker build -t quay.io/camillo/vkaci:1.0rc0 --target=vkaci .  &&  docker push quay.io/camillo/vkaci:1.0rc0 &&  docker push quay.io/camillo/vkaci-init:1.0rc0
+docker build -t quay.io/camillo/vkaci-init:$TAG --target=vkaci-init  . &&  docker build -t quay.io/camillo/vkaci:$TAG --target=vkaci .  &&  docker push quay.io/camillo/vkaci:$TAG &&  docker push quay.io/camillo/vkaci-init:$TAG
